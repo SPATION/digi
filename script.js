419d0f67c6f44caf7396bbbbed154005820b35fb
James Kevin Hubler <jkhubler@spation.net>
2019-03-05 23:35:36 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(e) {
+  document.querySelector('#test').innerHTML = 'Hello, JavaScript Anywhere!';
+}, false);
+var firstNumber = 1;
+var secondNumber = 2;
+firstNumber + secondNumber;
